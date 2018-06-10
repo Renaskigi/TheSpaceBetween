96a12fd82c31c4136de63bde4c8d0ebf6046e6a4
Ryan Lee <Renaskigi@protonmail.com>
2018-06-10 11:04:22 -0700

    api key hidden

--- Public/Scripts/Views/app.js
+++ Public/Scripts/Views/app.js
@@ -1,4 +1,8 @@
 'use strict';
+
+
 function apiKeyLoader() {
-        $('#api-key').attr("src","https://maps.googleapis.com/maps/api/js?key=" + config + "&callback=initMap");
+    const config = config.api_key;
+    $('#api-key').attr("src","https://maps.googleapis.com/maps/api/js?key=" + config + "&callback=initMap");
 }
+
