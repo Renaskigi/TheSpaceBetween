e890f11fa16b18f5fc7b45e25f5ac548ac78bf56
Davidchhing <davidhchhing@gmail.com>
2018-06-11 20:47:13 -0700

    cleaned up the space

--- Public/Scripts/Views/app.js
+++ Public/Scripts/Views/app.js
@@ -1,9 +1,1 @@
 'use strict';
-
-
-// function apiKeyLoader() {
-//     const config = config.api_key;
-//     $('#api-key').attr("src","https://maps.googleapis.com/maps/api/js?key=" + config + "&callback=initMap");
-//     $('#api-libraries').attr("src","https://maps.googleapis.com/maps/api/js?key=" + config + "&libraries = geometry,directions,places");
-// }
-// apiKeyLoader();
