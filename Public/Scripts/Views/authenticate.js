529ef30a43104497b8329291c18bfbb2310015bd
Ryan Lee <Renaskigi@protonmail.com>
2018-06-17 12:03:12 -0700

    got simple database authentication

--- Public/Scripts/Views/authenticate.js
+++ Public/Scripts/Views/authenticate.js
@@ -2,7 +2,7 @@
 
 let username;
 let userpass;
-$('#account-form').on("click", function(event) {
+$('#account-submit').on("click", function(event) {
     username = $('#account-username').val();
     userpass = $('#account-userpass').val();
     $.post('/account', {username: username, userpass: userpass})
@@ -18,3 +18,17 @@ $('#account-form').on("click", function(event) {
 });
 
 
+$('#newaccount-submit').on("click", function(event) {
+    username = $('#newaccount-username').val();
+    userpass = $('#newaccount-userpass').val();
+    $.post('/new-account', {username: username, userpass: userpass})
+    .then(function(data) {
+        const login = {
+            username : data[0].username,
+            id : data[0].id
+        }
+    localStorage.setItem("login", JSON.stringify(login))
+    window.location.href = "/";
+    })
+    .fail(console.error);
+});
